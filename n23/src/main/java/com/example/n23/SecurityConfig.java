fb02109cb519cebd2278d438d331883b346bee67
mournful <linayn9@gmail.com>
2021-03-27 12:58:45 +0300

    pr. 22 finally fixed

---
+++ n23/src/main/java/com/example/n23/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.example.n23;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
